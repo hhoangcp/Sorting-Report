b78aa84c65753699627355ad198838c5c94e5956
Hà Văn Hoàng <58208726+hhoangcpascal@users.noreply.github.com>
2022-03-12 16:27:41 +0700

    Create Merge_Sort.cpp

---
+++ File_Cpp/Merge_Sort.cpp
@@ -0,0 +1,54 @@
+/// hhoangcpascal
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <iomanip>
+#include <chrono>
+#include <cassert>
+
+using namespace std;
+using namespace chrono;
+
+double a[1000006], tmp[1000006];
+int n;
+
+void Merge_Sort(int Left, int Right) {
+    if (Left >= Right) return;
+    if (Left + 1 == Right) {
+        if (a[Left] > a[Right]) swap(a[Left], a[Right]);
+        return;
+    }
+
+    int Mid = (Left + Right) >> 1;
+    Merge_Sort(Left, Mid);
+    Merge_Sort(Mid+1, Right);
+
+    int len = Right - Left + 1;
+    merge(a + Left, a + Mid + 1, a + Mid + 1, a + Right + 1, tmp);
+    copy(tmp, tmp + len, a + Left);
+}
+
+int main() {
+    freopen("Sorting.inp", "r", stdin);
+    freopen("Sorting.out", "w", stdout);
+
+    ios_base::sync_with_stdio(NULL); cin.tie(NULL);
+
+    cin >> n;
+    for(int i = 0; i < n; ++i) cin >> a[i];
+
+    auto Start = high_resolution_clock::now();
+    Merge_Sort(0, n-1);
+    auto End = high_resolution_clock::now();
+
+    auto elapsed = duration_cast<milliseconds>(End - Start);
+    cerr << "Time to sort: " << elapsed.count();
+
+    for(int i = 0; i < n - 1; ++i) assert(a[i] <= a[i+1]);
+
+    for(int i = 0; i < n; ++i) cout << fixed << setprecision(6) << a[i] << ' ';
+
+    return 0;
+}
