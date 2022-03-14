6292380d0447b066acd5f670f62a6d482f105463
Hà Văn Hoàng <58208726+hhoangcpascal@users.noreply.github.com>
2022-03-14 13:33:08 +0700

    Update Merge_Sort.cpp

--- File_Cpp/Merge_Sort.cpp
+++ File_Cpp/Merge_Sort.cpp
@@ -11,10 +11,9 @@
 using namespace std;
 using namespace chrono;
 
-double a[1000006], tmp[1000006];
-int n;
+double tmp[1000006];
 
-void Merge_Sort(int Left, int Right) {
+void Merge_Sort(double a[], int Left, int Right) {
     if (Left >= Right) return;
     if (Left + 1 == Right) {
         if (a[Left] > a[Right]) swap(a[Left], a[Right]);
@@ -22,13 +21,16 @@ void Merge_Sort(int Left, int Right) {
     }
 
     int Mid = (Left + Right) >> 1;
-    Merge_Sort(Left, Mid);
-    Merge_Sort(Mid+1, Right);
+    Merge_Sort(a, Left, Mid);
+    Merge_Sort(a, Mid+1, Right);
 
     int len = Right - Left + 1;
     merge(a + Left, a + Mid + 1, a + Mid + 1, a + Right + 1, tmp);
     copy(tmp, tmp + len, a + Left);
 }
+
+double a[1000006];
+int n;
 
 int main() {
     freopen("Sorting.inp", "r", stdin);
@@ -40,7 +42,7 @@ int main() {
     for(int i = 0; i < n; ++i) cin >> a[i];
 
     auto Start = high_resolution_clock::now();
-    Merge_Sort(0, n-1);
+    Merge_Sort(a, 0, n-1);
     auto End = high_resolution_clock::now();
 
     auto elapsed = duration_cast<milliseconds>(End - Start);
